4f0e2b638cce58112082c6726cc2bad0f2d1c20c
Juan Nascimento <juannascimento9910@gmail.com>
2024-03-17 23:12:10 -0300

    Testes feitos em casa

--- domain/archive.py
+++ domain/archive.py
@@ -1,23 +1,31 @@
 import csv
 
+
 class Archive:
     def __init__(self, path_file):
         self.path_file = path_file
 
-    def create_archive(self, dados):
+    def create_archive(self):
+        dados = []
+        while True:
+            nome = input("Digite seu nome: ")
+            if nome.lower() == 'sair':
+                break
+
+            matricula = input("Digite sua matrícula: ")
+            if matricula.isdigit():  # Verifica se a entrada contém apenas dígitos
+                matricula = int(matricula)  # Converte para inteiro se for válido
+                curso = input("Digite o seu curso: ")
+                dados.append([nome, matricula, curso])
+            else:
+                print("Matrícula inválida. Digite Novamente.")
+
         with open(self.path_file, 'w', encoding="utf-8", newline='') as file:
             csvwriter = csv.writer(file, delimiter=',')
             for row in dados:
                 csvwriter.writerow(row)
 
-# Definindo os dados fora da classe
-dados = [
-    ['Nome', 'Matricula', 'Curso'],
-    ['Juan', 2504, 'ADS'],
-    ['Joãozinho', 1015, 'Sistemas'],
-    ['Maria', 2023, 'Redes']
-]
 
 # Instanciando a classe Archive e chamando o método create_archive
 archive = Archive('exemplo.csv')
-archive.create_archive(dados)
+archive.create_archive()
